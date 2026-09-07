132244fa454af6d53184be957b13612ea20c22cd
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-07 22:05:29 +0530

    specialVariables

--- conditions.sh
+++ conditions.sh
@@ -5,6 +5,9 @@ sum=10
 if [ $sum -eq 10 ]
 then
    echo "number is matching, we are good"
-else
-   echo " number is not matching, not sure we are good or not"
+else if [ $sum -gt 10 ]
+then
+   echo " number is greater than expected"
+else ]
+   echo " number is less than expected"
 fi
