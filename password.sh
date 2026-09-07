f748091d7835deeb2e9d085435eeee74b5ce527a
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-07 06:04:19 +0530

    shell-practice

---
+++ password.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Please enter your name"
+
+read name
+
+echo "Please choose your service"
+
+read service
+
+echo "please provide your email id to create an account for you"
+
+read email
+
+echo "please enter your password for account login"
+
+read -s password
+
+echo "hi $name, your account with email $email is opened in the site singamden.fun"
