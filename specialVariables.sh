024a14250cef1dc0d31d801ea3d6b5fcb0fe9ecd
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-07 21:55:44 +0530

    specialVariables

---
+++ specialVariables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "$1 is first argument"
+echo "$2 is second argument"
+echo "$@ are all the arguments" 
+echo "$# number of arguments"
+echo "$0 is the script name"
+echo "$pwd is the current script directory"
+echo "$HOME is the users home directory"
+echo "$$ is the PID of current script"
+echo "$! is the PID of the last running process"
+sleep 10 &
+echo "$? is the exit status of last command executed"
