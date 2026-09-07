98c98981787120829306b9025d5c7672ae8313fb
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-07 05:54:54 +0530

    shell-practice

--- hello-world.sh
+++ hello-world.sh
@@ -2,7 +2,9 @@
 
 person1=theeta
 person2=dabba
+date=$(date)
 
 echo "$person1 : how are you dabba..?"
 echo "$person2 : wonderful, i went dubai for trip and it is wonderful"
 echo "$person1 : many are telling that you just went to mumbai but telling everyone Dubai"
+echo "$person2 : no i just came back from dubai at $date"
