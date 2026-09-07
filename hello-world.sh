8551249968bb0a119a270b7e1b1644542533733f
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-07 05:36:03 +0530

    first commit

---
+++ hello-world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=theeta
+person2=dabba
+
+echo "$person1 : how are you dabba..?"
+echo "$person2 : wonderful, i went dubai for trip and it is wonderful"
+echo "$person1 : many are telling that you just went to mumbai but telling everyone Dubai"
