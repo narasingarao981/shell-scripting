cf24ae7c781ac4bb807adc3245f4d42c9add0e8f
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-07 22:19:55 +0530

    specialVariables

---
+++ installCommand.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+installation=$1
+
+dnf list installed $installation
+
+if [ $? -ne 0 ] ; then
+  echo "$installation is not installed. Proceeding with installation"
+  dnf install $installation
+
+  if [ $? -eq 0 ] ; then
+    echo "$installation installed.. SUCCESSFUL"
+  else
+    echo "$installation is failed.. FAILED"
+  fi
+else 
+  echo "$installation already exists... SKIPPED"
+
+fi
